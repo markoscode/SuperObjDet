b4289821c4bae35ae531386831e514c22a942118
Animesh Agrawal <animesh.a.777@gmail.com>
2023-07-03 16:31:22 +0800

    allow more range in depth elasticty

--- mask2former/modeling/transformer_decoder/mask2former_transformer_decoder.py
+++ mask2former/modeling/transformer_decoder/mask2former_transformer_decoder.py
@@ -272,7 +272,7 @@ class MultiScaleMaskedTransformerDecoder(nn.Module):
         self.mask_classification = mask_classification
 
         #weight sharing
-        self.DEPTH_LIST = [np.arange(3)]
+        self.DEPTH_LIST = [np.arange(6)]
         self.runtime_depth = 0
 
         # positional encoding
