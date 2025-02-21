7e855ac1673468ee8557b25334df09e697b25a1b
riksanyal <debopamsanyal@gmail.com>
2025-02-21 11:14:08 -0500

    finished porting engine files

--- mask2former/engine/train_loop.py
+++ mask2former/engine/train_loop.py
@@ -466,9 +466,9 @@ class WSTrainer(TrainerBase):
         for i in range(self.dynamic_bs):
             if i == 0:
                 #logger.info(f"setting to max_net {type(self.model)}, {type(self.model.module)}")
-                subnet_settings = self.model.module.backbone.set_max_net()
+                subnet_settings = self.model.module.set_max_net()
             else:
-                subnet_settings = self.model.module.backbone.sample_active_subnet()
+                subnet_settings = self.model.module.sample_active_subnet()
             logger.info(f"current subnet {subnet_settings}")
             #subnet_str += str(subnet_settings) + ", "
             loss_dict = self.model(data)
@@ -483,6 +483,184 @@ class WSTrainer(TrainerBase):
                     loss_dict_total[key] = 0
                 loss_dict_total[key] += value
             #wandb.log({f"loss for subnet {subnet_settings}": losses})
+        losses.backward()
+
+        self.after_backward()
+
+        self._write_metrics(loss_dict_total, data_time)
+
+        """
+        If you need gradient clipping/scaling or other processing, you can
+        wrap the optimizer with your custom `step()` method. But it is
+        suboptimal as explained in https://arxiv.org/abs/2006.15704 Sec 3.2.4
+        """
+        #logging.info(subnet_str)
+        self.optimizer.step()
+
+    @property
+    def _data_loader_iter(self):
+        # only create the data loader iterator when it is used
+        if self._data_loader_iter_obj is None:
+            self._data_loader_iter_obj = iter(self.data_loader)
+        return self._data_loader_iter_obj
+
+    def reset_data_loader(self, data_loader_builder):
+        """
+        Delete and replace the current data loader with a new one, which will be created
+        by calling `data_loader_builder` (without argument).
+        """
+        del self.data_loader
+        data_loader = data_loader_builder()
+        self.data_loader = data_loader
+        self._data_loader_iter_obj = None
+
+    def _write_metrics(
+        self,
+        loss_dict: Mapping[str, torch.Tensor],
+        data_time: float,
+        prefix: str = "",
+    ) -> None:
+        if (self.iter + 1) % self.gather_metric_period == 0:
+            SimpleTrainer.write_metrics(loss_dict, data_time, prefix)
+
+    @staticmethod
+    def write_metrics(
+        loss_dict: Mapping[str, torch.Tensor],
+        data_time: float,
+        prefix: str = "",
+    ) -> None:
+        """
+        Args:
+            loss_dict (dict): dict of scalar losses
+            data_time (float): time taken by the dataloader iteration
+            prefix (str): prefix for logging keys
+        """
+        metrics_dict = {k: v.detach().cpu().item() for k, v in loss_dict.items()}
+        metrics_dict["data_time"] = data_time
+
+        # Gather metrics among all workers for logging
+        # This assumes we do DDP-style training, which is currently the only
+        # supported method in detectron2.
+        all_metrics_dict = comm.gather(metrics_dict)
+
+        if comm.is_main_process():
+            storage = get_event_storage()
+
+            # data_time among workers can have high variance. The actual latency
+            # caused by data_time is the maximum among workers.
+            data_time = np.max([x.pop("data_time") for x in all_metrics_dict])
+            storage.put_scalar("data_time", data_time)
+
+            # average the rest metrics
+            metrics_dict = {
+                k: np.mean([x[k] for x in all_metrics_dict]) for k in all_metrics_dict[0].keys()
+            }
+            total_losses_reduced = sum(metrics_dict.values())
+            if not np.isfinite(total_losses_reduced):
+                raise FloatingPointError(
+                    f"Loss became infinite or NaN at iteration={storage.iter}!\n"
+                    f"loss_dict = {metrics_dict}"
+                )
+
+            storage.put_scalar("{}total_loss".format(prefix), total_losses_reduced)
+            if len(metrics_dict) > 1:
+                storage.put_scalars(**metrics_dict)
+
+    def state_dict(self):
+        ret = super().state_dict()
+        ret["optimizer"] = self.optimizer.state_dict()
+        return ret
+
+    def load_state_dict(self, state_dict):
+        super().load_state_dict(state_dict)
+        self.optimizer.load_state_dict(state_dict["optimizer"])
+
+#TODO: Update Stichnet training
+class SNNETTrainer(TrainerBase):
+    """
+    A simple trainer for the most common type of task:
+    single-cost single-optimizer single-data-source iterative optimization,
+    optionally using data-parallelism.
+    It assumes that every step, you:
+
+    1. Compute the loss with a data from the data_loader.
+    2. Compute the gradients with the above loss.
+    3. Update the model with the optimizer.
+
+    All other tasks during training (checkpointing, logging, evaluation, LR schedule)
+    are maintained by hooks, which can be registered by :meth:`TrainerBase.register_hooks`.
+
+    If you want to do anything fancier than this,
+    either subclass TrainerBase and implement your own `run_step`,
+    or write your own training loop.
+    """
+
+    def __init__(self, model, data_loader, optimizer, gather_metric_period=1):
+        """
+        Args:
+            model: a torch Module. Takes a data from data_loader and returns a
+                dict of losses.
+            data_loader: an iterable. Contains data to be used to call model.
+            optimizer: a torch optimizer.
+            gather_metric_period: an int. Every gather_metric_period iterations
+                the metrics are gathered from all the ranks to rank 0 and logged.
+        """
+        super().__init__()
+
+        """
+        We set the model to training mode in the trainer.
+        However it's valid to train a model that's in eval mode.
+        If you want your model (or a submodule of it) to behave
+        like evaluation during training, you can overwrite its train() method.
+        """
+        model.train()
+
+        self.model = model
+        self.data_loader = data_loader
+        # to access the data loader iterator, call `self._data_loader_iter`
+        self._data_loader_iter_obj = None
+        self.optimizer = optimizer
+        self.gather_metric_period = gather_metric_period
+
+    def run_step(self):
+        """
+        Implement the standard training logic described above.
+        """
+        logger = logging.getLogger(__name__)
+        assert self.model.training, "[SimpleTrainer] model was changed to eval mode!"
+        start = time.perf_counter()
+        """
+        If you want to do something with the data, you can wrap the dataloader.
+        """
+        data = next(self._data_loader_iter)
+        data_time = time.perf_counter() - start
+
+        """
+        If you need to accumulate gradients or do something similar, you can
+        wrap the optimizer with your custom `zero_grad()` method.
+        """
+        self.optimizer.zero_grad()
+
+        """
+        Sample random stich config
+        """
+        losses = 0
+        loss_dict_total = {}
+        stitchnet_id = self.model.module.backbone.sample_random_stitch_config()
+        logger.info(f"current config id {stitchnet_id}")
+        print(f"current config id {stitchnet_id}, comb: {self.model.module.backbone.stitch_configs[stitchnet_id]['comb_id']}")
+        loss_dict = self.model(data)
+        if isinstance(loss_dict, torch.Tensor):
+            losses += loss_dict
+            loss_dict = {"total_loss": loss_dict}
+        else:
+            losses += sum(loss_dict.values())
+
+        for key, value in loss_dict.items():
+            if key not in loss_dict_total:
+                loss_dict_total[key] = 0
+            loss_dict_total[key] += value
+
         losses.backward()
 
         self.after_backward()
